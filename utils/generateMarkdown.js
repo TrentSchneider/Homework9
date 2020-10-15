599d20f09a953980de5c44e751ebc0b2c1f5d5d3
Trent Schneider <trentonschneider@gmail.com>
2020-10-15 14:32:07 -0400

    updated template with additional content/functionality

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,42 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  const tocLic,
+  secLic;
+  if (data.needLicense === "Yes") {
+    tocLic = "[License](#lic)";
+    secLic = `<a id="lic"></a>
+    ## License
+    `
+  }
   return `# ${data.title}
+${data.link}
+## Description 
+${data.description}
 
+[Installation](#inst)
+[Usage](#use)
+${tocLic}
+[Contributing](#cont)
+[Tests](#tes)
+[Questions](#ques)
+
+<a id="inst"></a>
+## Installation
+${data.installation}
+<a id="use"></a>
+## Usage
+${data.usage}
+${secLic}
+${data.license}
+<a id="cont"></a>
+## Contributing
+${data.contributing}
+<a id="tes"></a>
+## Tests
+${data.tests}
+<a id="ques"></a>
+## Questions
+If you have any questions you can reach me on [GitHub](https://github.com/${data.github}) or via [email](mailto:${data.email}).
 `;
 }
 
